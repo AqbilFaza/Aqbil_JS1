e2c39d5ed47277a5bf683521b7ad6776b935b5b3
AqbilFaza <135787343+AqbilFaza@users.noreply.github.com>
2023-10-08 13:49:58 +0700

    Delete Muhammad Aqbil Faza Rachman_JS1 directory
    hapus file

--- Muhammad Aqbil Faza Rachman_JS1/forStatement.js
+++
@@ -1,9 +0,0 @@
-let tinggi = 5; // Ganti nilai ini dengan tinggi segitiga yang diinginkan
-
-for (let x = 1; x <= tinggi; x++) {
-  let baris = '';
-  for (let y = 1; y <= x; y++) {
-    baris += y + ' ';
-  }
-  console.log(baris);
-}
